7b3b508828771e0007943f13198d596b6c10eff2
rojiwon0325 <bond9986@naver.com>
2022-10-11 21:31:06 +0900

    company controller 적용

--- src/api/company/company.module.ts
+++ src/api/company/company.module.ts
@@ -4,9 +4,11 @@ import { CompanyService } from './application/company.service';
 import { CompanyEntity } from './infrastructure/company.entity';
 import { CompanyEntityMapper } from './infrastructure/company.mapper';
 import { CompanyRepository } from './infrastructure/company.repository';
+import { CompanyController } from './presentation/company.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CompanyEntity])],
   providers: [CompanyEntityMapper, CompanyRepository, CompanyService],
+  controllers: [CompanyController],
 })
 export class CompanyModule {}
