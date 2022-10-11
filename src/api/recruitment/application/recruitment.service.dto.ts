e6bd75240a8b7ef992df840b1fde072d5b627a37
rojiwon0325 <bond9986@naver.com>
2022-10-12 00:53:07 +0900

    Recruitment 상세 조회 & 목록 검색 기능 구현

--- src/api/recruitment/application/recruitment.service.dto.ts
+++ src/api/recruitment/application/recruitment.service.dto.ts
@@ -3,6 +3,7 @@ import {
   IRecruitmentId,
   IRecruitmentProperty,
 } from '../domain/recruitment.interface';
+import { FindManyOption } from '../domain/repository.interface';
 
 export type CreateRecruitmentDTO = Omit<
   IRecruitmentProperty,
@@ -10,5 +11,12 @@ export type CreateRecruitmentDTO = Omit<
 >;
 
 export type FindOneRecruitmentDTO = Pick<IRecruitmentProperty, 'id'>;
+
+export type FindManyRecruitmentDTO = FindManyOption;
+
+export type UpdateRecruitmentDTO = Pick<
+  IRecruitmentProperty,
+  'compensation' | 'description' | 'position' | 'skill'
+>;
 
 export type RemoveRecruitmentDTO = Pick<IRecruitmentProperty, 'id'>;
--- src/api/recruitment/application/recruitment.service.ts
+++ src/api/recruitment/application/recruitment.service.ts
@@ -1,12 +1,20 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Recruitment } from '../domain/recruitment.aggregate';
-import { IRecruitment } from '../domain/recruitment.interface';
-import { IRecruitmentRepository } from '../domain/repository.interface';
+import {
+  IRecruitment,
+  IRecruitmentDetailReponseType,
+  IRecruitmentResponseType,
+} from '../domain/recruitment.interface';
+import {
+  FindManyOption,
+  IRecruitmentRepository,
+} from '../domain/repository.interface';
 import { RecruitmentRepository } from '../infrastructure/recruitment.repository';
 import {
   CreateRecruitmentDTO,
   FindOneRecruitmentDTO,
   RemoveRecruitmentDTO,
+  UpdateRecruitmentDTO,
 } from './recruitment.service.dto';
 
 @Injectable()
@@ -30,6 +38,30 @@ export class RecruitmentService {
 
   async findOne(dto: FindOneRecruitmentDTO): Promise<IRecruitment> {
     return this.recruitmentRepository.findOne(dto.id);
+  }
+
+  async findOneDetail(
+    dto: FindOneRecruitmentDTO,
+  ): Promise<IRecruitmentDetailReponseType> {
+    const recruitment = await this.recruitmentRepository.findOne(dto.id, true);
+    const ids = await this.recruitmentRepository.findIds({
+      id: recruitment.id,
+      company_id: recruitment.company_id,
+    });
+    return recruitment.getDetailResponseType(ids);
+  }
+
+  async findMany(dto: FindManyOption): Promise<IRecruitmentResponseType[]> {
+    const entities = await this.recruitmentRepository.findMany(dto);
+    return entities.map((entity) => entity.getResponseType());
+  }
+
+  async update(
+    filter: FindOneRecruitmentDTO,
+    dto: UpdateRecruitmentDTO,
+  ): Promise<IRecruitment> {
+    const recruitment = await this.recruitmentRepository.findOne(filter.id);
+    return this.recruitmentRepository.save({ ...recruitment, ...dto });
   }
 
   async remove(dto: RemoveRecruitmentDTO): Promise<void> {
