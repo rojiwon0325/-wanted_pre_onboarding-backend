78b8ecc7fe4e0f1c696533328c8cf222a24efbc5
rojiwon0325 <bond9986@naver.com>
2022-10-12 00:53:46 +0900

    Recruitment 상세조회/목록검색/수정 API 구현

--- src/api/recruitment/presentation/recruitment.controller.dto.ts
+++ src/api/recruitment/presentation/recruitment.controller.dto.ts
@@ -1,12 +1,14 @@
-import { IRecruitmentReponseType } from '../domain/recruitment.interface';
+import { IRecruitmentResponseType } from '../domain/recruitment.interface';
+import { IRecruitmentDetailReponseType } from '../domain/recruitment.interface';
 import { Type } from 'class-transformer';
-import { IsNumber, IsString } from 'class-validator';
+import { IsInt, IsNumber, IsOptional, IsString } from 'class-validator';
 import { BaseAggregate } from 'src/api/common/model/aggregate.base';
 import {
   IRecruitmentId,
   IRecruitmentProperty,
 } from '../domain/recruitment.interface';
 import { RecruitmentErrorMessage } from '../infrastructure/recruitment.entity';
+import { OmitType } from '@nestjs/swagger';
 
 export class FindOneRecruitmentParam {
   @IsNumber()
@@ -15,19 +17,36 @@ export class FindOneRecruitmentParam {
 }
 
 export type FindOneRecruitmentResponse = {
-  Recruitment: IRecruitmentReponseType;
+  Recruitment: IRecruitmentDetailReponseType;
+};
+
+export class FindManyRecruitmentQuery {
+  @IsString()
+  @IsOptional()
+  search?: string;
+
+  @IsInt()
+  @IsOptional()
+  @Type(() => Number)
+  page?: number;
+}
+
+export type FindManyRecruitmentResponse = {
+  Recruitment: IRecruitmentResponseType[];
 };
 
 export class CreateRecruitmentBody
   implements Omit<IRecruitmentProperty, keyof BaseAggregate<IRecruitmentId>>
 {
   @IsNumber({}, { message: RecruitmentErrorMessage.company_id })
+  @Type(() => Number)
   company_id: number;
 
   @IsString({ message: RecruitmentErrorMessage.position })
   position: string;
 
   @IsNumber({}, { message: RecruitmentErrorMessage.compensation })
+  @Type(() => Number)
   compensation: number;
 
   @IsString({ message: RecruitmentErrorMessage.description })
@@ -37,7 +56,15 @@ export class CreateRecruitmentBody
   skill: string;
 }
 
+export class UpdateRecruitmentBody extends OmitType(CreateRecruitmentBody, [
+  'company_id',
+]) {}
+
 export type CreateRecruitmentResponse = {
+  Recruitment: IRecruitmentProperty;
+};
+
+export type UpdateRecruitmentResponse = {
   Recruitment: IRecruitmentProperty;
 };
 
--- src/api/recruitment/presentation/recruitment.controller.ts
+++ src/api/recruitment/presentation/recruitment.controller.ts
@@ -1,26 +1,56 @@
+import {
+  IRecruitmentDetailReponseType,
+  IRecruitmentResponseType,
+} from '../domain/recruitment.interface';
 import { IRecruitment } from '../domain/recruitment.interface';
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { RecruitmentService } from '../application/recruitment.service';
 import {
   CreateRecruitmentBody,
   CreateRecruitmentResponse,
+  FindManyRecruitmentQuery,
+  FindManyRecruitmentResponse,
   FindOneRecruitmentParam,
   FindOneRecruitmentResponse,
   RemoveRecruitmentResponse,
+  UpdateRecruitmentBody,
+  UpdateRecruitmentResponse,
 } from './recruitment.controller.dto';
 
 @Controller('Recruitment')
 export class RecruitmentController {
   constructor(private readonly recruitmentService: RecruitmentService) {}
 
+  @Get()
+  async findMany(
+    @Query() { search, page = 1 }: FindManyRecruitmentQuery,
+  ): Promise<FindManyRecruitmentResponse> {
+    const Recruitment: IRecruitmentResponseType[] =
+      await this.recruitmentService.findMany({
+        search,
+        page,
+      });
+    return { Recruitment };
+  }
+
   @Get(':recruitment_id')
   async findOne(
     @Param() { recruitment_id: id }: FindOneRecruitmentParam,
   ): Promise<FindOneRecruitmentResponse> {
-    const Recruitment: IRecruitment = await this.recruitmentService.findOne({
-      id,
-    });
-    return { Recruitment: Recruitment.getResponseType() };
+    const Recruitment: IRecruitmentDetailReponseType =
+      await this.recruitmentService.findOneDetail({
+        id,
+      });
+    return { Recruitment };
   }
 
   @Post()
@@ -35,6 +65,19 @@ export class RecruitmentController {
       position,
       skill,
     });
+    return { Recruitment };
+  }
+
+  @Patch(':recruitment_id')
+  async update(
+    @Param() { recruitment_id: id }: FindOneRecruitmentParam,
+    @Body() body: UpdateRecruitmentBody,
+  ): Promise<UpdateRecruitmentResponse> {
+    const { compensation, description, position, skill } = body;
+    const Recruitment: IRecruitment = await this.recruitmentService.update(
+      { id },
+      { position, compensation, description, skill },
+    );
     return { Recruitment };
   }
 
